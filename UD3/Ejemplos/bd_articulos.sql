90491fac5b61e07c25b482da8afd519d49b5651c
samuel L.C. <slcardoso.docente@gmail.com>
2025-11-17 10:11:51 +0100

    modifico bd de ejemplo mvc

--- UD3/Ejemplos/bd_articulos.sql
+++ UD3/Ejemplos/bd_articulos.sql
@@ -8,7 +8,18 @@ CREATE TABLE articulo(
     fecha DATE
 );
 
+CREATE TABLE resena(
+    cod_resena INT AUTO_INCREMENT PRIMARY KEY,
+    cod_articulo INT NOT NULL,
+    descripcion VARCHAR(256) NOT NULL,
+    fecha_hora DATETIME,
+    CONSTRAINT fk_resena_articulo FOREIGN KEY (cod_articulo) REFERENCES articulo(cod_articulo)
+
+);
+
 --Carga de datos
-INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Boli rojo', '2024-12-09');
-INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Boli azul', '2024-12-07');
-INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Boli negro', '2024-12-06');
+INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Boli rojo', '2024-07-09');
+INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Boli azul', '2025-10-07');
+INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Calculadora', '2024-11-06');
+INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Goma de borrar', '2025-12-06');
+INSERT INTO `articulo` (`cod_articulo`, `titulo`, `fecha`) VALUES (NULL, 'Folios', '2025-12-06');
