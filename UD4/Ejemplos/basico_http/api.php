e7bfa365703e26d0afb88bd9d81058dafa353fe2
samuel L.C. <slcardoso.docente@gmail.com>
2026-01-09 12:36:38 +0100

    Ejemplos básicos manejo peticiones HTTP con php

--- UD4/Ejemplos/basico_http/api.php
+++ UD4/Ejemplos/basico_http/api.php
@@ -4,4 +4,17 @@
  * Todo el trafico que reciba el servidor se redirige a este fichero.
  */
 
-echo "Redirige bien!";
+echo "Redirige bien! <br>";
+
+// Obtener URI de la petición HTTP
+$uri = $_SERVER['REQUEST_URI'];
+// Obtenemos el método HTTP
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Obtener cuerpo de la petición
+$body = file_get_contents('php://input');
+
+echo "uri = $uri<br>";
+echo "método = $method<br>";
+
+echo "cuerpo = $body";
