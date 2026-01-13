f521bd1c9f06bfa12da5782485791f666373f4b1
samuel L.C. <slcardoso.docente@gmail.com>
2026-01-13 13:40:49 +0100

    ejemplo de api básica completo

--- UD4/Ejemplos/escuelas/index.php
+++ UD4/Ejemplos/escuelas/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once "globals.php";
 use Ejemplos\escuelas\core\Request;
+use Ejemplos\escuelas\controller\MunicipioController;
+
 spl_autoload_register(function ($clase) {
 
     $ruta = $_SERVER['DOCUMENT_ROOT'] . '/' . str_replace('\\', '/', $clase) . '.php';
@@ -14,25 +16,41 @@ spl_autoload_register(function ($clase) {
 // 0. Declarar endpoints
 $endpoints = [
     // Endpoints de municipio
-    ['method'=>'GET','uri'=>'/municipios','handler'=>['MunicipioController','index']],
-    ['method'=>'GET','uri'=>'/municipios/{id}','handler'=>['MunicipioController','show']],
-    ['method'=>'DELETE','uri'=>'/municipios/{id}','handler'=>['MunicipioController','destroy']],
-    ['method'=>'UPDATE','uri'=>'/municipios/{id}','handler'=>['MunicipioController','update']],
-    ['method'=>'POST','uri'=>'/municipios','handler'=>['MunicipioController','store']],
-    
+    ['method' => 'GET', 'uri' => '/municipios', 'handler' => [MunicipioController::class, 'index']],
+    ['method' => 'GET', 'uri' => '/municipios/{id}', 'handler' => [MunicipioController::class, 'show']],
+    ['method' => 'DELETE', 'uri' => '/municipios/{id}', 'handler' => [MunicipioController::class, 'destroy']],
+    ['method' => 'UPDATE', 'uri' => '/municipios/{id}', 'handler' => [MunicipioController::class, 'update']],
+    ['method' => 'POST', 'uri' => '/municipios', 'handler' => [MunicipioController::class, 'store']],
+
 ];
 
 // 1 Obtener el método, uri y body de la peticion
 $request = new Request();
 
 // 2. Localizar el endpoint.
-    // Recorro todos los endpoints declarados
-    foreach($endpoints as $route){
-        // Compruebo si el método coincide
-        if($route['method'] == $request->method()){
-            // Compruebo si la uri encaja
+// Recorro todos los endpoints declarados
+foreach ($endpoints as $route) {
+    // Compruebo si el método coincide
+    if ($route['method'] == $request->method()) {
+        // Creo el patron para ese endpoint concreto.
+        $patern = '#^' . preg_replace('#\{[\w]+\}#', '([\w]+)', $route['uri']) . '$#';
+        // Para /municipios/{id}
+        //  $patern = '#^/municipios/([\w]+)$#'
+        // Para /municipios
+        //  $patern = '#^/municipios$#'
+        // Para /municipios/{id}/calles/4
+        //  $patern = '#^/municipios/([\w]+)/calles/([\w]+)$#'
+        
+        // Compruebo si la uri encaja
+        if (preg_match($patern, $request->uri(), $matches)) {
+            unset($matches[0]);
+            // Llamar al método del controlador adecuado
+            $controller = new $route['handler'][0]();
+            call_user_func_array([$controller, $route['handler'][1]], $matches);
         }
-        
+
     }
-        
+
+}
+
 // 3. Llamar al método del controlador correspondiente.
