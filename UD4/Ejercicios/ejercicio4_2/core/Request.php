aaeae943a11a82d83d54771015e11622d3c6d56a
samuel L.C. <slcardoso.docente@gmail.com>
2026-02-04 09:33:54 +0100

    Ejercicio 4.2 terminado con validate

--- UD4/Ejercicios/ejercicio4_2/core/Request.php
+++ UD4/Ejercicios/ejercicio4_2/core/Request.php
@@ -36,7 +36,7 @@ class Request
      */
     public function body(): array
     {
-        return json_decode(file_get_contents('php://input'),true);
+        return json_decode(file_get_contents('php://input'), true);
     }
 
     /**
@@ -57,8 +57,68 @@ class Request
      */
     public function validate(array $rules)
     {
-        //TODO
+        $data = $this->body();
+        $errors = [];
 
+        foreach ($rules as $field => $ruleString) {
+            $rulesArray = explode('|', $ruleString);
+            foreach ($rulesArray as $rule) {
+                //Required
+                if ($rule === 'required' && !isset($data[$field])) {
+                    $errors[$field][] = "Campo obligatorio";
+                    continue;
+                }
+                //Tipo de dato (string, int y numeric)
+                if ($rule === 'string' && isset($data[$field]) && !is_string($data[$field])) {
+                    $errors[$field][] = "Campo debe ser un string";
+                    continue;
+                }
+
+                if ($rule === 'int' && isset($data[$field]) && !is_int($data[$field])) {
+                    $errors[$field][] = "Campo debe ser un número entero";
+                    continue;
+                }
+
+                if ($rule === 'numeric' && isset($data[$field]) && !is_numeric($data[$field])) {
+                    $errors[$field][] = "Campo debe ser un número";
+                    continue;
+                }
+                //Limites de magnitud
+                if (str_starts_with($rule, 'max:')) {
+                    $max = (int) explode(':', $rule)[1];
+                    $dataLen = is_string($data[$field]) ? strlen($data[$field]) : $data[$field];
+                    if (isset($data[$field]) && $dataLen > $max) {
+                        $errors[$field][] = "El campo no puede ser mayor de $max";
+                        continue;
+                    }
+                }
+
+                if (str_starts_with($rule, 'min:')) {
+                    $min = (int) explode(':', $rule)[1];
+                    $dataLen = is_string($data[$field]) ? strlen($data[$field]) : $data[$field];
+                    if (isset($data[$field]) && $dataLen < $min) {
+                        $errors[$field][] = "El campo no puede ser menor de $min";
+                        continue;
+                    }
+                }
+
+            }
+
+        }
+
+        //Comprobamos si hay errores (no valida)
+        if (!empty($errors)) {
+            Response::json(
+                [
+                    'message' => 'Datos invalidos'
+                    ,
+                    'errors' => $errors
+                ]
+                ,
+                400
+            );
+            exit;
+        }
     }
 
 
