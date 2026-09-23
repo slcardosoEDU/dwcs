4f1c5b6bdd46dba37b03d2e6d8fd4d82daac18bd
Samuel Loureiro Cardoso <slcardoso.docente@gmail.com>
2026-09-23 09:33:21 +0200

    ejercicio 1.4

---
+++ UD1/src/ejercicios/actividad1/ejercicio4.php
@@ -0,0 +1,22 @@
+<?php
+define("PI", 3.1416);
+function volumenCilindro(float $r, float $h): float
+{
+    return $r ** 2 * $h * PI;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <h1>Ejercicio 4</h1>
+    Volumen de un cilindro de radio 5cm y altura 20.5cm es: 
+    <span style="color: red"><?= volumenCilindro(5, 20.5) ?>cm</span>;
+</body>
+
+</html>
